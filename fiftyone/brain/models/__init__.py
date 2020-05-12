8c78b55079da0ae00e22b8270405a959c521ee02
Jason Corso <jason@voxel51.com>
2020-05-12 16:49:27 -0400

    creating a local brain.models package for custom models we need

---
+++ fiftyone/brain/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+FiftyOneBrain: the protected methods in FiftyOne
+A powerful package for dataset curation, analysis, and visualization.
+
+See https://github.com/voxel51/fiftyone for more information.
+
+| Copyright 2017-2020, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
