fdc55cdbb9d310d2bc886586db6c8697718462a6
Jason Corso <jason@voxel51.com>
2020-05-18 18:07:37 -0400

    added the prediction and embedding functions

--- fiftyone/brain/uniqueness.py
+++ fiftyone/brain/uniqueness.py
@@ -23,10 +23,17 @@ import os.path
 import numpy as np
 import torch
 from torch import nn
+import torchvision
 
 from fiftyone.brain.models.simple_resnet import *
 
 import fiftyone.core.insights as foi
+import fiftyone.utils.torch as fout
+
+
+# @todo consider moving these outside to some brain utils or config
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+cpu = torch.device("cpu")
 
 
 def compute_uniqueness(data, key_label, key_insight=None, validate=False):
@@ -71,16 +78,78 @@ def compute_uniqueness(data, key_label, key_insight=None, validate=False):
     model = Network(simple_resnet()).to(device).half()
     model.load_state_dict(torch.load(MODEL_PATH))
 
-    # should be part of the specific model class
+    loader = _make_data_loader(data)
 
+    model.train(False)
+    with torch.no_grad():
+        for imgs, sample_ids in loader:
+            vectors = _embed(model, imgs)
+
+            # take the vectors and then compute knn on them
+
+    #    insight = foi.ScalarInsight.create(name="hardness", scalar=hardness)
+    #    sample.add_insight(key_insight, insight)
+
+
+def _make_data_loader(data, batch_size=16):
+    """Makes a data loader that can be used for getting the dataset off-disk
+    and processed by our model.
+
+    XXX @todo should the class that ultimately wraps the model/weights be
+    required to supply a data loader function like this?
+
+    Args:
+        data: an iterable of :class:`fiftyone.core.sample.Sample` instances
+        batch_size: the int size of the batches in the loader
+    """
+    mean = [0.4914, 0.4822, 0.4465]
+    std = [0.2023, 0.1994, 0.2010]
+    transforms = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize([32, 32]),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(mean, std),
+        ]
+    )
+
+    # XXX should the fiftyone view be able to supply this operation directly?
+    image_paths = []
+    sample_ids = []
     for sample in data:
-        label = sample.get_label(key_label)
+        image_paths.append(sample.filepath)
+        sample_ids.append(sample.id)
+
+    dataset = fout.TorchImageDataset(
+        image_paths, sample_ids=sample_ids, transform=transforms
+    )
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                       num_workers=4)
 
 
-        hardness = entropy(_softmax(np.asarray(label.logits)))
+def _predict(model, imgs):
+    """Computes a prediction on the imgs using the model.
 
-        insight = foi.ScalarInsight.create(name="hardness", scalar=hardness)
-        sample.add_insight(key_insight, insight)
+    @todo should be part of the actual model instance representation
+    """
+    inputs = dict(input=imgs.cuda().half())
+    outputs = model(inputs)
+    logits = outputs['logits'].detach().cpu().numpy()
+    predictions = np.argmax(logits, axis=1)
+    odds = np.exp(logits)
+    confidences = np.max(odds, axis=1) / np.sum(odds, axis=1)
+    return predictions, confidences, logits
+
+
+def _embed(model, imgs):
+    """Embeds the imgs into the model's space.
+
+    @todo should be in the actual model instance representation
+
+    XXX unclear if should be flatten or linear;
+    """
+    inputs = dict(input=imgs.cuda().half())
+    outputs = model(inputs)
+    return outputs['flatten'].detach().cpu().numpy()
 
 
 def _validate(data, key_label):
