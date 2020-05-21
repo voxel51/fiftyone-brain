f07086dbda65e28c41e6dbb75614ab3650f4f68d
brimoor <brimoor@umich.edu>
2020-05-21 15:18:47 -0400

    updating hardness method to use new interface

--- fiftyone/brain/hardness.py
+++ fiftyone/brain/hardness.py
@@ -1,5 +1,5 @@
 """
-Definitions of methods that compute insights related to sample hardness.
+Methods that compute insights related to sample hardness.
 
 | Copyright 2017-2020, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
@@ -21,13 +21,12 @@ from builtins import *
 import numpy as np
 from scipy.stats import entropy
 
-import fiftyone.core.insights as foi
 
-
-def compute_hardness(data, key_label, key_insight=None, validate=False):
-    """Adds a ``hardness`` :class:`fiftyone.core.insight.ScalarInsight` to each
-    sample scoring the difficulty that ``key_label`` observed in classifying
-    the sample.
+def compute_hardness(
+    samples, label_field, hardness_field="hardness", validate=False
+):
+    """Adds a hardness field to each sample scoring the difficulty that the
+    specified label field observed in classifying the sample.
 
     Hardness is a measure computed based on model prediction output that
     summarizes a measure of the uncertainty the model had with the sample.
@@ -35,36 +34,34 @@ def compute_hardness(data, key_label, key_insight=None, validate=False):
     hard samples, annotation errors during noisy training, and more.
 
     Args:
-        data: an iterable of :class:`fiftyone.core.sample.Sample` instances
-        key: string denoting what group label to operate for getting the label
-            prediction information and for adding the insight
-        key_insight (None): string denoting the group for the insight
-            denotation to be specified only if different than `key`
+        samples: an iterable of :class:`fiftyone.core.sample.Sample` instances
+        label_field: the :class:`fiftyone.core.labels.Classification` label
+            field to use from each sample
+        hardness_field ("hardness"): the field name to use to store the
+            hardness value for each sample
         validate (False): whether to validate that the provided samples have
             the required fields to be processed
     """
-    # **Algorithm:** hardness is computed directly as the entropy of the logits.
+    #
+    # Algorithm
+    #
+    # Hardness is computed directly as the entropy of the logits.
+    #
     if validate:
-        _validate(data, key_label)
+        _validate(samples, label_field)
 
-    if key_insight is None:
-        key_insight = key_label
-
-    for sample in data:
-        label = sample.get_label(key_label)
-
+    for sample in samples:
+        label = sample[label_field]
         hardness = entropy(_softmax(np.asarray(label.logits)))
-
-        insight = foi.ScalarInsight.create(name="hardness", scalar=hardness)
-        sample.add_insight(key_insight, insight)
+        sample[hardness_field] = hardness
 
 
-def _validate(data, key_label):
+def _validate(samples, label_field):
     """Validates that all samples in the dataset are usable for the hardness
     computation.
     """
-    for sample in data:
-        label = sample.get_label(key_label)
+    for sample in samples:
+        label = sample[label_field]
         if label.logits is None:
             raise ValueError(
                 "Sample '%s' failed `compute_hardness` validation because it "
