56afafb150b595c9e844cce3d6383cf298121e1d
brimoor <brimoor@umich.edu>
2020-05-22 01:35:56 -0400

    using imageio instead of scipy to load images

--- fiftyone/brain/models/tests/test_simple_resnet.py
+++ fiftyone/brain/models/tests/test_simple_resnet.py
@@ -18,7 +18,7 @@ from builtins import *
 # pragma pylint: enable=unused-wildcard-import
 # pragma pylint: enable=wildcard-import
 
-import scipy.misc as spm
+import imageio
 from PIL import Image
 import torch
 import torchvision
@@ -30,9 +30,11 @@ import fiftyone.brain as fob
 import fiftyone.zoo as foz
 import fiftyone.core.odm as foo
 
+
 def transpose(x, source, target):
     """Simple transpose function for Tensors."""
     return x.permute([source.index(d) for d in target])
+
 
 foo.drop_database()
 dataset = foz.load_zoo_dataset("cifar10", split="test")
@@ -43,16 +45,20 @@ print("Working on image at %s" % filepath)
 
 im_pil = Image.open(filepath)
 print(f"im_pil is type {type(im_pil)}")
-im_spy = spm.imread(filepath)
-print(f"im_spy is type {type(im_spy)}")
-print(im_spy.shape)
-im_tor = torch.from_numpy(im_spy)
-im_tor = transpose(im_tor, "HWC", "CHW")
-print(f"im_tor is type {type(im_tor)}")
-print(im_tor.shape)
+
+im_numpy = imageio.imread(filepath)
+print(f"im_numpy is type {type(im_numpy)}")
+print(im_numpy.shape)
+
+im_torch = torch.from_numpy(im_numpy)
+im_torch = transpose(im_torch, "HWC", "CHW")
+print(f"im_torch is type {type(im_torch)}")
+print(im_torch.shape)
+
 im_eta = etai.read(filepath)
 print(f"im_eta is type {type(im_eta)}")
 print(im_eta.shape)
+
 im_et2 = im_eta / 255
 print(f"im_et2 is type {type(im_et2)}")
 print(im_et2.shape)
@@ -65,13 +71,13 @@ print("PIL")
 p = model.predict(im_pil)
 print(p[0])
 
-print("SPY")
-p = model.predict(im_spy)
+print("IMAGEIO")
+p = model.predict(im_numpy)
 print(p[0])
 
-print("TOR")
+print("TORCH")
 try:
-    p = model.predict(im_tor)
+    p = model.predict(im_torch)
     print(p[0])
 except NotImplementedError:
     print("successfully raised error on torch image")
@@ -84,5 +90,5 @@ print("ET2")
 p = model.predict(im_et2)
 print(p[0])
 
-#@todo logging a need to understand why the predictions on the ETA images are
-#different than those on the other formats. -- just by a confidence level
+# @todo logging a need to understand why the predictions on the ETA images are
+# different than those on the other formats. -- just by a confidence level
