53a704f7e75b2d0b661a2140bf66289f5709fd67
brimoor <brimoor@umich.edu>
2020-05-22 01:33:00 -0400

    adding progress bar to track computation

--- fiftyone/brain/uniqueness.py
+++ fiftyone/brain/uniqueness.py
@@ -18,33 +18,33 @@ from builtins import *
 # pragma pylint: enable=unused-wildcard-import
 # pragma pylint: enable=wildcard-import
 
-import os.path
+import logging
+import os
 
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import torch
 
 import eta.core.learning as etal
+import eta.core.utils as etau
 
 import fiftyone.utils.torch as fout
 
 
-# @todo consider moving these outside to some brain utils or config
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-cpu = torch.device("cpu")
+logger = logging.getLogger(__name__)
 
 
 def compute_uniqueness(samples, uniqueness_field="uniqueness", validate=False):
     """Adds a uniqueness field to each sample scoring how unique it is with
     respect to the rest of the samples.
 
-    This function only uses the pixel data and can process labeled or unlabeled
-    data.
+    This function only uses the pixel data and can therefore process labeled or
+    unlabeled samples.
 
     Args:
         samples: an iterable of :class:`fiftyone.core.sample.Sample` instances
         uniqueness_field ("uniqueness"): the field name to use to store the
-        uniqueness value for each samples.
+            uniqueness value for each sample
         validate (False): whether to validate that the provided samples have
             the required fields to be processed
     """
@@ -59,6 +59,7 @@ def compute_uniqueness(samples, uniqueness_field="uniqueness", validate=False):
     # than, say, "representativeness" which would stress samples that are core
     # to dense clusters of related samples.
     #
+
     # @todo convert to a parameter with a default, for tuning
     K = 3
 
@@ -67,66 +68,75 @@ def compute_uniqueness(samples, uniqueness_field="uniqueness", validate=False):
 
     model = etal.load_default_deployment_model("simple_resnet_cifar10")
 
-    loader = _make_data_loader(samples, model.transforms)
+    data_loader = _make_data_loader(samples, model.transforms)
 
+    # embeds will be `num_samples x dim`
     embeds = None
-    with torch.no_grad():
-        for imgs, _ in loader:
-            # @todo the existence of model.embed_all is not well engineered
-            vectors = model.embed_all(imgs)
 
-            if embeds is None:
-                embeds = vectors
-            else:
-                # @todo: if speed is an issue, fix this...
-                embeds = np.vstack((embeds, vectors))
+    num_samples = len(samples)
+    logger.info("Computing uniqueness for %d samples...", num_samples)
+    with etau.ProgressBar(len(samples), iters_str="samples") as progress:
+        with torch.no_grad():
+            for imgs in data_loader:
+                # @todo the existence of model.embed_all is not well engineered
+                vectors = model.embed_all(imgs)
 
-    # each row of embeddings is a sample from the dataset (via row index)
-    # dists and indices have a useless first column ("itself")
-    knn = NearestNeighbors(n_neighbors=K+1, algorithm="ball_tree").fit(embeds)
+                if embeds is None:
+                    embeds = vectors
+                else:
+                    # @todo if speed is an issue, fix this...
+                    embeds = np.vstack((embeds, vectors))
+
+                progress.set_iteration(progress.iteration + len(imgs))
+
+    logger.info("Analyzing samples...")
+
+    # first column of dists and indices is self-distance
+    knn = NearestNeighbors(n_neighbors=K + 1, algorithm="ball_tree").fit(
+        embeds
+    )
     dists, indices = knn.kneighbors(embeds)
 
-    assert indices.shape[1] == K+1
-
+    #
     # @todo experiment on which method for assessing uniqueness is best
-    # to get something going, for now, just take a weight mean
+    #
+    # To get something going, for now, just take a weighted mean
+    #
     weights = [0.6, 0.3, 0.1]
-    dists = dists[:, 1:]
-    dists *= weights
-    value_dist = dists.mean(1)
+    sample_dists = np.mean(dists[:, 1:] * weights, axis=1)
 
-    # need to normalize to keep the user on common footing across datasets
-    value_dist /= value_dist.max()
+    # Normalize to keep the user on common footing across datasets
+    sample_dists /= sample_dists.max()
 
-    for index, sample in enumerate(samples.iter_samples()):
-        sample[uniqueness_field] = value_dist[index]
+    for sample, val in zip(samples, sample_dists):
+        sample[uniqueness_field] = val
+        sample.save()
 
-    # @todo should these functions return anything?
+    logger.info("Uniqueness computation complete")
 
 
-def _make_data_loader(data, transforms, batch_size=16):
+def _make_data_loader(samples, transforms, batch_size=16):
     """Makes a data loader that can be used for getting the dataset off-disk
     and processed by our model.
 
-    XXX @todo should the class that ultimately wraps the model/weights be
-    required to supply a data loader function like this?
+    @todo should the class that ultimately wraps the model/weights be required
+    to supply a data loader function like this?
 
     Args:
-        data: an iterable of :class:`fiftyone.core.sample.Sample` instances
+        samples: an iterable of :class:`fiftyone.core.sample.Sample` instances
         transforms: a torchvision Transform sequence
-        batch_size: the int size of the batches in the loader
+        batch_size (16): the int size of the batches in the loader
     """
     image_paths = []
     sample_ids = []
-    for sample in data:
+    for sample in samples:
         image_paths.append(sample.filepath)
         sample_ids.append(sample.id)
 
-    dataset = fout.TorchImageDataset(
-        image_paths, sample_ids=sample_ids, transform=transforms
+    dataset = fout.TorchImageDataset(image_paths, transform=transforms)
+    return torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, num_workers=4
     )
-    return torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                       num_workers=4)
 
 
 def _validate(samples):
