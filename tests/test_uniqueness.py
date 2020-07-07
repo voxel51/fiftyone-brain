e824625696c14bfb73bb69844dde7aec3aa81f29
Jason Corso <jason@voxel51.com>
2020-07-07 17:47:18 -0400

    adds functionality to test gray image handling with drive data

--- tests/test_uniqueness.py
+++ tests/test_uniqueness.py
@@ -18,6 +18,11 @@ from builtins import *
 # pragma pylint: enable=unused-wildcard-import
 # pragma pylint: enable=wildcard-import
 
+import os.path
+
+import eta.core.storage as etas
+import eta.core.utils as etau
+
 import fiftyone.brain as fob
 import fiftyone.zoo as foz
 import fiftyone.core.odm as foo
@@ -38,14 +43,28 @@ def test_uniqueness():
 
 
 def test_gray():
-    """Test default support for handling grayscale images."""
+    """Test default support for handling grayscale images.
+
+    Uses a test data zip at this location:
+    https://drive.google.com/file/d/1ECeNnLmKQCHxlVdRqGefV5eXOD_OkmWx/view?usp=sharing
+
+    Requires Google Drive Voxel51 credentials to work; see the
+    eta/docs/storage_dev_guide.md.
+    """
     foo.drop_database()
 
-    dataset = Dataset.from_dir(
-        "/home/jason/Downloads/data/test", fot.ImageDirectory
-    )
+    with etau.TempDir() as tempdir:
+        tmp_zip = os.path.join(tempdir, "data.zip")
+        tmp_data = os.path.join(tempdir, "brain_grayscale_test_data")
+        client = etas.GoogleDriveStorageClient()
+        client.download("1ECeNnLmKQCHxlVdRqGefV5eXOD_OkmWx", tmp_zip)
+        etau.extract_zip(tmp_zip, delete_zip=True)
 
-    print(dataset.summary())
+        dataset = Dataset.from_dir(tmp_data, fot.ImageDirectory)
+
+        fob.compute_uniqueness(dataset)
+
+        print(dataset.summary())
 
 
 if __name__ == "__main__":
