f7dd797584f30fdaa4edffb5e617dd3bd67c953a
brimoor <brimoor@umich.edu>
2020-08-22 21:51:00 -0400

    optimizing mistakenness by selecting only necessary fields

--- fiftyone/brain/mistakenness.py
+++ fiftyone/brain/mistakenness.py
@@ -11,7 +11,9 @@ from math import exp
 import numpy as np
 from scipy.stats import entropy
 
-import eta.core.utils as etau
+import fiftyone.core.collections as foc
+import fiftyone.core.labels as fol
+import fiftyone.core.utils as fou
 
 
 logger = logging.getLogger(__name__)
@@ -44,7 +46,7 @@ def compute_mistakenness(
         mistakenness_field ("mistakenness"): the field name to use to store the
             mistakenness value for each sample
         validate (False): whether to validate that the provided samples have
-            the required fields to be processed
+            the required fields prior to processing them
     """
     #
     # Algorithm
@@ -61,18 +63,18 @@ def compute_mistakenness(
     # based on whether or not the answer is correct. $m = 1$ when the label is
     # correct and $0$ otherwise. Then, mistakenness is computed as $exp(m * c)$
     #
+
     if validate:
         _validate(samples, pred_field, label_field)
 
-    num_samples = len(samples)
-    logger.info("Computing mistakenness for %d samples...", num_samples)
-    with etau.ProgressBar(iters_str="samples") as progress:
-        for sample in progress(samples):
+    logger.info("Computing mistakenness...")
+    with fou.ProgressBar() as pb:
+        for sample in pb(_optimize(samples, [pred_field, label_field])):
             label = sample[pred_field]
             check = sample[label_field]
 
-            c = -1 * entropy(_softmax(np.asarray(label.logits)))
-            m = 1 if label.label == check.label else 0
+            c = -1.0 * entropy(_softmax(np.asarray(label.logits)))
+            m = 1.0 if label.label == check.label else 0.0
             mistakenness = exp(m * c)
 
             sample[mistakenness_field] = mistakenness
@@ -82,28 +84,53 @@ def compute_mistakenness(
 
 
 def _validate(samples, pred_field, label_field):
-    """Validates that all samples in the dataset are usable for the
-    mistakenness computation.
-    """
-    for sample in samples:
-        label = sample[pred_field]
-        if label.logits is None:
-            raise ValueError(
-                "Sample '%s' failed `compute_mistakenness` validation because "
-                "it has no logits" % sample.id
-            )
+    logger.info("Validating samples...")
+    with fou.ProgressBar() as pb:
+        for sample in pb(_optimize(samples, [pred_field, label_field])):
+            pred_label = sample[pred_field]
+            label = sample[label_field]
 
-        label = sample[label_field]
-        if label is None:
-            raise ValueError(
-                "Sample '%s' failed `compute_mistakenness` validation because "
-                "it has no '%s' label" % (sample.id, label_field)
-            )
+            if not isinstance(pred_label, fol.Classification):
+                raise ValueError(
+                    "Sample '%s' failed validation because its '%s' field is "
+                    "not a %s instance; expected %s"
+                    % (
+                        sample.id,
+                        pred_field,
+                        pred_label.__class__,
+                        fol.Classification,
+                    )
+                )
+
+            if pred_label.logits is None:
+                raise ValueError(
+                    "Sample '%s' failed validation because its '%s' field has "
+                    "no logits" % (sample.id, pred_field)
+                )
+
+            if not isinstance(label, fol.Classification):
+                raise ValueError(
+                    "Sample '%s' failed validation because its '%s' field is "
+                    "not a %s instance; expected %s"
+                    % (
+                        sample.id,
+                        label_field,
+                        label.__class__,
+                        fol.Classification,
+                    )
+                )
 
 
 def _softmax(npa):
-    """Computes softmax on the numpy array."""
-    # @todo Replace with ``scipy.special.softmax`` after upgrading to scipy as
-    #       it is more numerically stable.
+    # @todo replace with ``scipy.special.softmax`` after upgrading to scipy as
+    # it is more numerically stable
     a = np.exp(npa)
     return a / sum(a)
+
+
+def _optimize(samples, fields=None):
+    # Selects only the requested fields (and always the default fields)
+    if isinstance(samples, foc.SampleCollection):
+        return samples.select_fields(fields)
+
+    return samples
