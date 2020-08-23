d8c8a4784c6b6db713fffafbf319b51a711f5101
brimoor <brimoor@umich.edu>
2020-08-22 21:51:26 -0400

    optimizing hardness by selecting only necessary fields

--- fiftyone/brain/hardness.py
+++ fiftyone/brain/hardness.py
@@ -10,7 +10,9 @@ import logging
 import numpy as np
 from scipy.stats import entropy
 
-import eta.core.utils as etau
+import fiftyone.core.collections as foc
+import fiftyone.core.labels as fol
+import fiftyone.core.utils as fou
 
 
 logger = logging.getLogger(__name__)
@@ -34,20 +36,20 @@ def compute_hardness(
         hardness_field ("hardness"): the field name to use to store the
             hardness value for each sample
         validate (False): whether to validate that the provided samples have
-            the required fields to be processed
+            the required fields prior to processing them
     """
     #
     # Algorithm
     #
     # Hardness is computed directly as the entropy of the logits
     #
+
     if validate:
         _validate(samples, label_field)
 
-    num_samples = len(samples)
-    logger.info("Computing hardness for %d samples...", num_samples)
-    with etau.ProgressBar(iters_str="samples") as progress:
-        for sample in progress(samples):
+    logger.info("Computing hardness...")
+    with fou.ProgressBar() as pb:
+        for sample in pb(samples):
             label = sample[label_field]
             hardness = entropy(_softmax(np.asarray(label.logits)))
             sample[hardness_field] = hardness
@@ -57,21 +59,40 @@ def compute_hardness(
 
 
 def _validate(samples, label_field):
-    """Validates that all samples in the dataset are usable for the hardness
-    computation.
-    """
-    for sample in samples:
-        label = sample[label_field]
-        if label.logits is None:
-            raise ValueError(
-                "Sample '%s' failed `compute_hardness` validation because it "
-                "has no logits" % sample.id
-            )
+    logger.info("Validating samples...")
+    with fou.ProgressBar() as pb:
+        for sample in pb(_optimize(samples, label_field)):
+            label = sample[label_field]
+
+            if not isinstance(label, fol.Classification):
+                raise ValueError(
+                    "Sample '%s' failed validation because its '%s' field is "
+                    "not a %s instance; expected %s"
+                    % (
+                        sample.id,
+                        label_field,
+                        label.__class__,
+                        fol.Classification,
+                    )
+                )
+
+            if label.logits is None:
+                raise ValueError(
+                    "Sample '%s' failed validation because its '%s' field has "
+                    "no logits" % (sample.id, label)
+                )
 
 
 def _softmax(npa):
-    """Computes softmax on the numpy array."""
-    # @todo Replace with ``scipy.special.softmax`` after upgrading to scipy as
-    #       it is more numerically stable.
+    # @todo replace with ``scipy.special.softmax`` after upgrading to scipy as
+    # it is more numerically stable
     a = np.exp(npa)
     return a / sum(a)
+
+
+def _optimize(samples, fields=None):
+    # Selects only the requested fields (and always the default fields)
+    if isinstance(samples, foc.SampleCollection):
+        return samples.select_fields(fields)
+
+    return samples
