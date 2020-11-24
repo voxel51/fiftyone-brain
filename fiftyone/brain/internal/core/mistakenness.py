5b33f0e572ce090b8de03b1a969fa868dc60b9ee
Eric Hofesmann <ehofesmann@gmail.com>
2020-11-24 17:54:40 -0500

    roi mistakenness

--- fiftyone/brain/internal/core/mistakenness.py
+++ fiftyone/brain/internal/core/mistakenness.py
@@ -16,13 +16,18 @@ import fiftyone.core.collections as foc
 import fiftyone.core.labels as fol
 import fiftyone.core.utils as fou
 
+import fiftyone.utils.eval as foue
+
 import fiftyone.brain.internal.core.utils as fbu
 
 
 logger = logging.getLogger(__name__)
 
 
-_ALLOWED_TYPES = (fol.Classification, fol.Classifications)
+_ALLOWED_TYPES = (fol.Classification, fol.Classifications, fol.Detections)
+_MISSED_DETECTION_THRESHOLD = 0.2
+_DETECTION_IOU = 0.5
+_DETECTION_IOU_STR = str(_DETECTION_IOU).replace(".", "_")
 
 
 def compute_mistakenness(
@@ -77,26 +82,104 @@ def compute_mistakenness(
 
     samples = fbu.optimize_samples(samples, fields=(pred_field, label_field))
 
+    if samples and isinstance(next(iter(samples))[pred_field], fol.Detections):
+        foue.evaluate_detections(
+            samples,
+            pred_field,
+            label_field,
+            save_sample_fields=False,
+            classwise=False,
+            iou=_DETECTION_IOU,
+        )
+
     logger.info("Computing mistakenness...")
     with fou.ProgressBar() as pb:
         for sample in pb(samples):
             pred_label, label = _get_data(sample, pred_field, label_field)
 
-            if isinstance(pred_label, fol.Classifications):
-                # For multilabel problems, all labels must match
-                pred_labels = set(c.label for c in pred_label.classifications)
-                labels = set(c.label for c in label.classifications)
-                m = float(pred_labels == labels)
+            if isinstance(pred_label, fol.Detections):
+                possible_mistakes = 0
+                sample_mistakenness = []
+                pred_map = {}
+                for pred_det in pred_label.detections:
+                    pred_map[pred_det.id] = pred_det
+                    ent = entropy(softmax(np.asarray(pred_det["logits"])))
+                    gt_id = pred_det[label_field + "_eval"]["matches"][
+                        _DETECTION_IOU_STR
+                    ]["gt_id"]
+                    if gt_id == -1 and ent < _MISSED_DETECTION_THRESHOLD:
+                        pred_det["possible_mistake"] = "missed_detection"
+                        possible_mistakes += 1
+
+                for gt_det in label.detections:
+                    matches = gt_det[pred_field + "_eval"]["matches"]
+                    pred_id = matches[_DETECTION_IOU_STR]["pred_id"]
+                    iou = matches[_DETECTION_IOU_STR]["iou"]
+                    if pred_id == -1:
+                        gt_det["possible_mistake"] = "incorrect_annotation"
+                        possible_mistakes += 1
+
+                    else:
+                        pred_det = pred_map[pred_id]
+                        m = float(gt_det["label"] == pred_det["label"])
+                        mistakenness_class = _compute_mistakenness_class(
+                            pred_det["logits"], m
+                        )
+                        mistakenness_loc = _compute_mistakenness_loc(
+                            pred_det["logits"], iou
+                        )
+
+                        gt_det[mistakenness_field + "_loc"] = mistakenness_loc
+                        gt_det[mistakenness_field] = mistakenness_class
+                        sample_mistakenness.append(mistakenness_class)
+
+                sample[mistakenness_field] = np.mean(sample_mistakenness)
+                sample["possible_mistakes"] = possible_mistakes
+
             else:
-                m = float(pred_label.label == label.label)
+                if isinstance(pred_label, fol.Classifications):
+                    # For multilabel problems, all labels must match
+                    pred_labels = set(
+                        c.label for c in pred_label.classifications
+                    )
+                    labels = set(c.label for c in label.classifications)
+                    m = float(pred_labels == labels)
+                else:
+                    m = float(pred_label.label == label.label)
 
-            c = -1.0 * entropy(softmax(np.asarray(pred_label.logits)))
-            mistakenness = exp(m * c)
+                mistakenness = _compute_mistakenness_class(
+                    pred_label.logits, m
+                )
+                sample[mistakenness_field] = mistakenness
 
-            sample[mistakenness_field] = mistakenness
             sample.save()
 
     logger.info("Mistakenness computation complete")
+
+
+def _compute_mistakenness_class(logits, m):
+    # constrain m to either 1 (incorrect) or -1 (correct)
+    m = m * -2.0 + 1.0
+
+    c = -1.0 * entropy(softmax(np.asarray(logits)))
+    mistakenness = (m * exp(c) + 1.0) / 2.0
+
+    return mistakenness
+
+
+def _compute_mistakenness_loc(logits, iou):
+    # i = 0 for high iou, i = 1 for low iou
+    i = (1.0 / (1.0 - _DETECTION_IOU)) * (1.0 - iou)
+
+    # c = 0 for low confidence, c = 1 for high confidence
+    c = exp(-1.0 * entropy(softmax(np.asarray(logits))))
+
+    # mistakenness = i when c = i, mistakenness = 0.5 if c = 0
+    # mistakenness is higher with lower IoU and closer to 0 or 1 with higher
+    # confidence
+    mistakenness = (c * ((2.0 * i) - 1.0) + 1.0) / 2.0
+
+    return mistakenness
 
 
 def _get_data(sample, pred_field, label_field):
@@ -108,9 +191,19 @@ def _get_data(sample, pred_field, label_field):
         allow_none=False,
     )
 
-    if pred_label.logits is None:
-        raise ValueError(
-            "Sample '%s' field '%s' has no logits" % (sample.id, pred_field)
-        )
+    if isinstance(pred_label, fol.Detections):
+        for det in pred_label.detections:
+            if det.logits is None:
+                raise ValueError(
+                    "A detection in Sample '%s' field '%s' has no logits"
+                    % (sample.id, pred_field)
+                )
+
+    else:
+        if pred_label.logits is None:
+            raise ValueError(
+                "Sample '%s' field '%s' has no logits"
+                % (sample.id, pred_field)
+            )
 
     return pred_label, label
