9d1146da4a85b6bf9467841caf54f6451a0be02b
Benjamin Kane <ben@voxel51.com>
2020-12-14 08:13:33 -0700

    add python 3.9 distributions

--- setup.py
+++ setup.py
@@ -125,6 +125,6 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     scripts=[],
-    python_requires=">=3.5,<3.9",
+    python_requires=">=3.5",
     cmdclass={"build": CustomBuild, "bdist_wheel": CustomBdistWheel,},
 )
