dc5c9d7e851fefaec7f49f61768e32a8305ee34f
brimoor <brimoor@umich.edu>
2021-02-22 11:12:49 -0500

    adding basic interface unit tests

---
+++ tests/test_basic.py
@@ -0,0 +1,107 @@
+"""
+Basic brain method tests.
+
+| Copyright 2017-2021, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+import unittest
+
+import fiftyone as fo
+import fiftyone.brain as fob
+import fiftyone.zoo as foz
+
+
+def test_uniqueness():
+    dataset = foz.load_zoo_dataset("quickstart").clone()
+
+    fob.compute_uniqueness(dataset)
+    print(dataset.get_brain_info("uniqueness"))
+    print(dataset.bounds("uniqueness"))
+
+    print(dataset.list_brain_keys())
+    dataset.delete_brain_results()
+    print(dataset)
+
+
+def test_detection_mistakenness():
+    dataset = foz.load_zoo_dataset("quickstart").clone()
+
+    fob.compute_mistakenness(dataset, "predictions")
+    print(dataset.list_brain_keys())
+    print(dataset.get_brain_info("mistakenness"))
+    print(dataset.bounds("mistakenness"))
+    print(dataset.bounds("possible_missing"))
+    print(dataset.bounds("possible_spurious"))
+    print(dataset.bounds("ground_truth.detections.mistakenness"))
+    print(dataset.bounds("ground_truth.detections.mistakenness_loc"))
+    print(dataset.count_values("ground_truth.detections.possible_spurious"))
+    print(dataset.count_values("predictions.detections.possible_missing"))
+
+    dataset.delete_brain_results()
+    print(dataset)
+
+
+def test_classification_mistakenness_confidence():
+    dataset = foz.load_zoo_dataset("quickstart").clone()
+    test_view = dataset.take(10)
+
+    # labels proxy
+    model = foz.load_zoo_model("alexnet-imagenet-torch")
+    test_view.apply_model(model, "alexnet")
+
+    # predictions proxy
+    model = foz.load_zoo_model("resnet50-imagenet-torch")
+    test_view.apply_model(model, "resnet50")
+
+    fob.compute_mistakenness(test_view, "resnet50", label_field="alexnet")
+    print(dataset.list_brain_keys())
+    print(dataset.bounds("mistakenness"))
+    print(dataset.load_brain_view("mistakenness"))
+
+    dataset.delete_brain_results()
+    print(dataset)
+
+
+def test_classification_mistakenness_logits():
+    dataset = foz.load_zoo_dataset("quickstart").clone()
+    test_view = dataset.take(10)
+
+    # labels proxy
+    model = foz.load_zoo_model("alexnet-imagenet-torch")
+    test_view.apply_model(model, "alexnet")
+
+    # predictions proxy
+    model = foz.load_zoo_model("resnet50-imagenet-torch")
+    test_view.apply_model(model, "resnet50", store_logits=True)
+
+    fob.compute_mistakenness(
+        test_view, "resnet50", label_field="alexnet", use_logits=True
+    )
+    print(dataset.list_brain_keys())
+    print(dataset.bounds("mistakenness"))
+    print(dataset.load_brain_view("mistakenness"))
+
+    dataset.delete_brain_results()
+    print(dataset)
+
+
+def test_hardness():
+    dataset = foz.load_zoo_dataset("quickstart").clone()
+    test_view = dataset.take(10)
+    model = foz.load_zoo_model("alexnet-imagenet-torch")
+    test_view.apply_model(model, "alexnet", store_logits=True)
+
+    fob.compute_hardness(test_view, "alexnet")
+    print(dataset.list_brain_keys())
+    print(dataset.get_brain_info("hardness"))
+    print(dataset.bounds("hardness"))
+    print(dataset.load_brain_view("hardness"))
+
+    dataset.delete_brain_results()
+    print(dataset)
+
+
+if __name__ == "__main__":
+    fo.config.show_progress_bars = True
+    unittest.main(verbosity=2)
