03d379ca421670c995583c629feb41c72a04bb6d
brimoor <brimoor@umich.edu>
2021-03-22 15:26:37 -0400

    refactor public interface into visible location; consolidate embeddings parameters

--- fiftyone/brain/internal/core/visualization.py
+++ fiftyone/brain/internal/core/visualization.py
@@ -14,13 +14,14 @@ import sklearn.manifold as skm
 import eta.core.utils as etau
 
 import fiftyone.core.brain as fob
-import fiftyone.core.plots as fop
-import fiftyone.core.utils as fou
 import fiftyone.core.validation as fov
 import fiftyone.zoo as foz
 
-umap = fou.lazy_import(
-    "umap", callback=lambda: etau.ensure_package("umap-learn")
+from fiftyone.brain.visualization import (
+    VisualizationResults,
+    UMAPVisualizationConfig,
+    TSNEVisualizationConfig,
+    PCAVisualizationConfig,
 )
 
 
@@ -34,9 +35,8 @@ _DEFAULT_BATCH_SIZE = 16
 
 def compute_visualization(
     samples,
-    embeddings,
     patches_field,
-    embeddings_field,
+    embeddings,
     brain_key,
     num_dims,
     method,
@@ -51,10 +51,16 @@ def compute_visualization(
 
     fov.validate_collection(samples)
 
-    if model is None and embeddings_field is None and embeddings is None:
+    if model is None and embeddings is None:
         model = foz.load_zoo_model(_DEFAULT_MODEL_NAME)
         if batch_size is None:
             batch_size = _DEFAULT_BATCH_SIZE
+
+    if etau.is_str(embeddings):
+        embeddings_field = embeddings
+        embeddings = None
+    else:
+        embeddings_field = None
 
     config = _parse_config(
         config, embeddings_field, patches_field, method, num_dims, **kwargs
@@ -94,7 +100,7 @@ def compute_visualization(
             embeddings = np.stack(embeddings)
 
     if isinstance(embeddings, dict):
-        embeddings = [embeddings[_id] for _id in samples._get_sample_ids()]
+        embeddings = [embeddings[_id] for _id in samples.values("id")]
         embeddings = [e for e in embeddings if e is not None and e.size > 0]
         embeddings = np.concatenate(embeddings, axis=0)
 
@@ -107,108 +113,11 @@ def compute_visualization(
     return results
 
 
-class VisualizationResults(fob.BrainResults):
-    """Class for visualizing the results of :meth:`compute_visualization`.
-
-    Args:
-        samples: the :class:`fiftyone.core.collections.SampleCollection` for
-            which this visualization was computed
-        points: a ``num_samples x num_dims`` array of visualization points
-        config: the :class:`VisualizationConfig` used to generate the points
-    """
-
-    def __init__(self, samples, points, config):
-        self._samples = samples
-        self.points = points
-        self.config = config
-
-    def visualize(
-        self,
-        field=None,
-        labels=None,
-        classes=None,
-        backend=None,
-        show=True,
-        **kwargs,
-    ):
-        """Generates a scatterplot of the visualization results.
-
-        This method supports 2D or 3D visualizations, but interactive point
-        selection is only aviailable in 2D.
-
-        You can use the ``field`` or ``labels`` parameters to define a coloring
-        for the points.
-
-        You can connect this method to a :class:`fiftyone.core.session.Session`
-        in order to automatically sync the session's view with the currently
-        selected points in the plot.
-
-        Args:
-            field (None): a sample field or ``embedded.field.name`` to use to
-                color the points. Can be numeric or strings
-            labels (None): a list of numeric or string values to use to color
-                the points
-            classes (None): an optional list of classes whose points to plot.
-                Only applicable when ``labels`` contains strings
-            backend (None): the plotting backend to use. Supported values are
-                ``("plotly", "matplotlib")``. If no backend is specified, the
-                best applicable backend is chosen
-            show (True): whether to show the plot
-            **kwargs: keyword arguments for the backend plotting method:
-
-                -   "plotly" backend: :meth:`fiftyone.core.plots.plotly.scatterplot`
-                -   "matplotlib" backend: :meth:`fiftyone.core.plots.matplotlib.scatterplot`
-
-        Returns:
-            one of the following:
-
-            -   an :class:`fiftyone.core.plots.base.InteractivePlot`, if the
-                backend supports interactivity
-            -   a plotly or matplotlib figure, otherwise
-        """
-        return fop.scatterplot(
-            self.points,
-            samples=self._samples,
-            label_field=self.config.patches_field,
-            field=field,
-            labels=labels,
-            classes=classes,
-            backend=backend,
-            show=show,
-            **kwargs,
-        )
-
-    @classmethod
-    def _from_dict(cls, d, samples):
-        points = np.array(d["points"])
-        config = VisualizationConfig.from_dict(d["config"])
-        return cls(samples, points, config)
-
-
-class VisualizationConfig(fob.BrainMethodConfig):
-    """Base class for configuring :class:`Visualization` instances.
-
-    Args:
-        embeddings_field (None): the sample field containing the embeddings
-        patches_field (None): the sample field defining the patches we're
-            visualizing
-        num_dims (2): the dimension of the visualization space
-    """
-
-    def __init__(
-        self, embeddings_field=None, patches_field=None, num_dims=2, **kwargs
-    ):
-        super().__init__(**kwargs)
-        self.embeddings_field = embeddings_field
-        self.patches_field = patches_field
-        self.num_dims = num_dims
-
-
 class Visualization(fob.BrainMethod):
     """Base class for embedding visualization methods.
 
     Args:
-        config: a :class:`VisualizationConfig`
+        config: a :class:`fiftyone.brain.visualization.VisualizationConfig`
     """
 
     def fit(self, embeddings):
@@ -233,85 +142,42 @@ class Visualization(fob.BrainMethod):
         pass
 
 
-class TSNEVisualizationConfig(VisualizationConfig):
-    """Configuration for a :class:`TSNEVisualization`.
-
-    Args:
-        embeddings_field (None): the sample field containing the embeddings
-        patches_field (None): the sample field defining the patches we're
-            visualizing
-        num_dims (2): the dimension of the visualization space
-        pca_dims (50): the number of PCA dimensions to compute prior to running
-            t-SNE. It is highly recommended to reduce the number of dimensions
-            to a reasonable number (e.g. 50) before running t-SNE, as this will
-            suppress some noise and speed up the computation of pairwise
-            distances between samples
-        metric ("euclidean"): the metric to use when calculating distance
-            between embeddings. Must be a supported value for the ``metric``
-            argument of ``scipy.spatial.distance.pdist``
-        perplexity (30.0): the perplexity to use. Perplexity is related to the
-            number of nearest neighbors that is used in other manifold learning
-            algorithms. Larger datasets usually require a larger perplexity.
-            Typical values are in ``[5, 50]``
-        learning_rate (200.0): the learning rate to use. Typical values are
-            in ``[10, 1000]``. If the learning rate is too high, the data may
-            look like a ball with any point approximately equidistant from its
-            nearest neighbours. If the learning rate is too low, most points
-            may look compressed in a dense cloud with few outliers. If the cost
-            function gets stuck in a bad local minimum increasing the learning
-            rate may help
-        max_iters (1000): the maximum number of iterations to run. Should be at
-            least 250
-        seed (None): a random seed
-        verbose (False): whether to log progress
-    """
-
-    def __init__(
-        self,
-        embeddings_field=None,
-        patches_field=None,
-        num_dims=2,
-        pca_dims=50,
-        metric="euclidean",
-        perplexity=30.0,
-        learning_rate=200.0,
-        max_iters=1000,
-        seed=None,
-        verbose=False,
-        **kwargs,
-    ):
-        super().__init__(
-            embeddings_field=embeddings_field,
-            patches_field=patches_field,
-            num_dims=num_dims,
-            **kwargs,
-        )
-        self.pca_dims = pca_dims
-        self.metric = metric
-        self.perplexity = perplexity
-        self.learning_rate = learning_rate
-        self.max_iters = max_iters
-        self.seed = seed
-        self.verbose = verbose
-
-    @property
-    def method(self):
-        return "tsne"
-
-
-class TSNEVisualization(Visualization):
-    """t-distributed Stochastic Neighbor Embedding (t-SNE) embedding
+class UMAPVisualization(Visualization):
+    """Uniform Manifold Approximation and Projection (UMAP) embedding
     visualization.
 
     Args:
-        config: a :class:`TSNEVisualizationConfig`
+        config: a :class:`fiftyone.brain.visualization.UMAPVisualizationConfig`
+    """
+
+    def fit(self, embeddings):
+        _ensure_umap()
+
+        import umap
+
+        _umap = umap.UMAP(
+            n_components=self.config.num_dims,
+            n_neighbors=self.config.num_neighbors,
+            metric=self.config.metric,
+            min_dist=self.config.min_dist,
+            random_state=self.config.seed,
+            verbose=self.config.verbose,
+        )
+        return _umap.fit_transform(embeddings)
+
+
+class TSNEVisualization(Visualization):
+    """t-distributed Stochastic Neighbor Embedding (t-SNE) visualization.
+
+    Args:
+        config: a :class:`fiftyone.brain.visualization.TSNEVisualizationConfig`
     """
 
     def fit(self, embeddings):
         if self.config.pca_dims is not None:
             _pca = skd.PCA(
                 n_components=self.config.pca_dims,
-                svd_solver="randomized",
+                svd_solver=self.config.svd_solver,
                 random_state=self.config.seed,
             )
             embeddings = _pca.fit_transform(embeddings)
@@ -333,78 +199,20 @@ class TSNEVisualization(Visualization):
         return _tsne.fit_transform(embeddings)
 
 
-class UMAPVisualizationConfig(VisualizationConfig):
-    """Configuration for a :class:`UMAPVisualization`.
+class PCAVisualization(Visualization):
+    """Principal component analysis (PCA) embedding visualization.
 
     Args:
-        embeddings_field (None): the sample field containing the embeddings
-        patches_field (None): the sample field defining the patches we're
-            visualizing
-        num_dims (2): the dimension of the visualization space
-        num_neighbors (15): the number of neighboring points used in local
-            approximations of manifold structure. Larger values will result i
-             more global structure being preserved at the loss of detailed
-             local structure. Typical values are in ``[5, 50]``
-        metric ("euclidean"): the metric to use when calculating distance
-            between embeddings. See `https://github.com/lmcinnes/umap`_ for
-            supported values
-        min_dist (0.1): the effective minimum distance between embedded
-            points. This controls how tightly the embedding is allowed compress
-            points together. Larger values ensure embedded points are more
-            evenly distributed, while smaller values allow the algorithm to
-            optimise more accurately with regard to local structure. Typical
-            values are in ``[0.001, 0.5]``
-        seed (None): a random seed
-        verbose (False): whether to log progress
-    """
-
-    def __init__(
-        self,
-        embeddings_field=None,
-        patches_field=None,
-        num_dims=2,
-        num_neighbors=15,
-        metric="euclidean",
-        min_dist=0.1,
-        seed=None,
-        verbose=False,
-        **kwargs,
-    ):
-        super().__init__(
-            embeddings_field=embeddings_field,
-            patches_field=patches_field,
-            num_dims=num_dims,
-            **kwargs,
-        )
-        self.num_neighbors = num_neighbors
-        self.metric = metric
-        self.min_dist = min_dist
-        self.seed = seed
-        self.verbose = verbose
-
-    @property
-    def method(self):
-        return "umap"
-
-
-class UMAPVisualization(Visualization):
-    """Uniform Manifold Approximation and Projection (UMAP) embedding
-    visualization.
-
-    Args:
-        config: a :class:`UMAPVisualizationConfig`
+        config: a :class:`fiftyone.brain.visualization.PCAVisualizationConfig`
     """
 
     def fit(self, embeddings):
-        _umap = umap.UMAP(
+        _pca = skd.PCA(
             n_components=self.config.num_dims,
-            n_neighbors=self.config.num_neighbors,
-            metric=self.config.metric,
-            min_dist=self.config.min_dist,
+            svd_solver=self.config.svd_solver,
             random_state=self.config.seed,
-            verbose=self.config.verbose,
         )
-        return _umap.fit_transform(embeddings)
+        return _pca.fit_transform(embeddings)
 
 
 def _parse_config(
@@ -413,10 +221,12 @@ def _parse_config(
     if config is not None:
         return config
 
-    if method is None or method == "tsne":
-        config_cls = TSNEVisualizationConfig
-    elif method == "umap":
+    if method is None or method == "umap":
         config_cls = UMAPVisualizationConfig
+    elif method == "tsne":
+        config_cls = TSNEVisualizationConfig
+    elif method == "pca":
+        config_cls = PCAVisualizationConfig
     else:
         raise ValueError("Unsupported method '%s'" % method)
 
@@ -426,3 +236,15 @@ def _parse_config(
         num_dims=num_dims,
         **kwargs,
     )
+
+
+def _ensure_umap():
+    try:
+        etau.ensure_package("umap-learn")
+    except:
+        raise ImportError(
+            "You must install the `umap-learn` package in order to use "
+            "UMAP-based visualization. This is recommended, as UMAP is "
+            "awesome! If you do not wish to install UMAP, try `method='tsne'` "
+            "instead"
+        )
