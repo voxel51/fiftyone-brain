42b30aef5e18a047f8f3b824796753d54ffd09a8
Jacob Marks <jacobmarks@Jacob-Voxel-Macbook.attlocal.net>
2023-02-15 19:24:44 -0800

    [WIP] pinecone backend - NOT COMPLETE

--- fiftyone/brain/internal/core/similarity.py
+++ fiftyone/brain/internal/core/similarity.py
@@ -18,6 +18,9 @@ from fiftyone.brain.similarity import (
     SimilarityResults,
 )
 import fiftyone.brain.internal.core.utils as fbu
+import fiftyone.core.utils as fou
+
+pinecone = fou.lazy_import("pinecone")
 
 
 logger = logging.getLogger(__name__)
@@ -26,6 +29,449 @@ _AGGREGATIONS = {"mean": np.mean, "min": np.min, "max": np.max}
 
 _MAX_PRECOMPUTE_DISTS = 15000  # ~1.7GB to store distance matrix in-memory
 _COSINE_HACK_ATTR = "_cosine_hack"
+
+class PineconeSimilarityConfig(SimilarityConfig):
+    """Configuration for the pinecone similarity backend.
+
+    Args:
+        embeddings_field (None): the sample field containing the embeddings,
+            if one was provided
+        model (None): the :class:`fiftyone.core.models.Model` or class name of
+            the model that was used to compute embeddings, if one was provided
+        patches_field (None): the sample field defining the patches being
+            analyzed, if any
+        supports_prompts (False): whether this run supports prompt queries
+        metric ("euclidean"): the embedding distance metric to use. Supported 
+            values are "euclidean", "cosine", and "dotproduct".
+    """
+    def __init__(
+        self,
+        embeddings_field=None,
+        model=None,
+        patches_field=None,
+        supports_prompts=None,
+        metric="euclidean",
+        index_name="testname",
+        dimension=None,
+        pod_type="p1",
+        pods=1,
+        replicas=1,
+        api_key=None,
+        environment=None,
+        upsert_pagination=100,
+        **kwargs,
+    ):
+        super().__init__(
+            embeddings_field=embeddings_field,
+            model=model,
+            patches_field=patches_field,
+            supports_prompts=supports_prompts,
+            **kwargs,
+        )
+        self.metric = metric
+        self.index_name = index_name
+        self.dimension = dimension
+        self.pod_type = pod_type
+        self.pods = pods
+        self.replicas = replicas
+        self.api_key = api_key
+        self.environment = environment
+        self.upsert_pagination = upsert_pagination
+
+    @property
+    def method(self):
+        return "pinecone"
+
+class PineconeSimilarity(Similarity):
+    """Pinecone similarity class for similarity backends.
+
+    Args:
+        config: an :class:`PineconeSimilarityConfig`
+    """
+
+    def ensure_requirements(self):
+        ## do I need "pinecone" here?
+        pass
+
+    def initialize(self, samples):
+        return PineconeSimilarityResults(samples, self.config, backend=self)
+
+    def cleanup(self, samples, brain_key):
+        pass
+
+class PineconeSimilarityResults(SimilarityResults):
+    """Class for interacting with pinecone similarity results.
+
+    Args:
+        samples: the :class:`fiftyone.core.collections.SampleCollection` used
+        config: the :class:`SimilarityConfig` used
+        embeddings (None): a ``num_embeddings x num_dims`` array of embeddings
+        sample_ids (None): a ``num_embeddings`` array of sample IDs
+        label_ids (None): a ``num_embeddings`` array of label IDs, if
+            applicable
+        backend (None): a :class:`PineconeSimilarity` instance
+    """
+
+    def __init__(
+        self,
+        samples,
+        config,
+        embeddings=None,
+        sample_ids=None,
+        label_ids=None,
+        backend=None,
+    ):
+        embeddings, sample_ids, label_ids = self._parse_data(
+            samples,
+            config,
+            embeddings=embeddings,
+            sample_ids=sample_ids,
+            label_ids=label_ids,
+        )
+
+        dimension = self._parse_dimension(
+            embeddings,
+            config
+        )
+
+        self._embeddings = embeddings
+        self._dimension = dimension
+        self._sample_ids = sample_ids
+        self._label_ids = label_ids
+        self._index_name = config.index_name
+        self._pod_type = config.pod_type
+        self._pods = config.pods
+        self._replicas = config.replicas
+        self._metric = config.metric   
+        self._upsert_pagination = config.upsert_pagination 
+        self._api_key = config.api_key
+        self._environment = config.environment
+        
+        print("Initializing pinecone index")
+        pinecone.init(config.api_key, config.environment)
+        pinecone.create_index(
+            self._index_name, 
+            dimension=self._dimension, 
+            metric=self._metric, 
+            pod_type=self._pod_type,
+            pods=self._pods,
+            replicas=self._replicas
+        )
+        print("Populating pinecone index")
+
+        self._neighbors_helper = None
+
+        super().__init__(samples, config, backend=backend)
+
+    @property
+    def sample_ids(self):
+        """The sample IDs of the full index."""
+        return self._sample_ids
+
+    @property
+    def label_ids(self):
+        """The label IDs of the full index, or ``None`` if not applicable."""
+        return self._label_ids
+
+    def remove_from_index(
+        self,
+        sample_ids=None,
+        label_ids=None,
+        allow_missing=True,
+        warn_missing=False,
+    ):
+        pass
+
+    # def _
+
+    def _to_inds(self, ids):
+        pass
+
+    def _sort_by_similarity(
+        self, query, k, reverse, aggregation, return_dists
+    ):
+        pass
+
+    def add_to_index(
+        self,
+        embeddings,
+        sample_ids,
+        label_ids=None,
+        overwrite=True,
+        allow_existing=True,
+        warn_existing=False,
+    ):
+
+        embeddings_list = [arr.tolist() for arr in embeddings]
+        if label_ids is not None:
+            index_vectors = list(zip(label_ids, embeddings_list, {"sample_id": sid for sid in sample_ids}))
+        else:
+            index_vectors = list(zip(sample_ids, embeddings_list))
+        
+        num_vectors = embeddings.shape[0]
+        num_steps = int(np.ceil(num_vectors / self._upsert_pagination))
+
+        pinecone.init(
+            api_key=self._api_key, 
+            environment=self._environment, 
+        )
+        index = pinecone.Index(self._index_name)
+
+        for i in range(num_steps):
+            min_ind = self._upsert_pagination * i
+            max_ind = min(self._upsert_pagination * (i+1), num_vectors)
+            index.upsert(index_vectors[min_ind:max_ind])
+
+    def attributes(self):
+        attrs = super().attributes()
+
+        if self.config.embeddings_field is not None:
+            attrs = [
+                attr
+                for attr in attrs
+                if attr not in ("embeddings", "sample_ids", "label_ids")
+            ]
+
+        return attrs
+
+    def _parse_dimension(self, embeddings, config):
+        print(embeddings.shape)
+        if config.dimension is not None:
+            return int(config.dimension)
+        elif embeddings is not None:
+            return int(embeddings.shape[1])
+        return 0
+
+    # def _reload(self, hard=False):
+    #     if hard:
+    #         # @todo reload embeddings from gridFS too?
+    #         # @todo `_samples` is not not declared in SimilarityResults API
+    #         if self.config.embeddings_field is not None:
+    #             embeddings, sample_ids, label_ids = self._parse_data(
+    #                 self._samples,
+    #                 self.config,
+    #             )
+
+    #             self._embeddings = embeddings
+    #             self._sample_ids = sample_ids
+    #             self._label_ids = label_ids
+    #             self._neighbors_helper = None
+
+    #     self.use_view(self._curr_view)
+
+   
+
+    # def _radius_neighbors(self, query=None, thresh=None, return_dists=False):
+    #     neighbors, _ = self._get_neighbors()
+
+    #     # When not using brute force, we approximate cosine distance by
+    #     # computing Euclidean distance on unit-norm embeddings.
+    #     # ED = sqrt(2 * CD), so we need to scale the threshold appropriately
+    #     if getattr(neighbors, _COSINE_HACK_ATTR, False):
+    #         thresh = np.sqrt(2.0 * thresh)
+
+    #     query_inds = self._to_inds(query)
+    #     if query_inds is not None:
+    #         _query = self._embeddings[query_inds, :]
+    #     else:
+    #         _query = query
+
+    #     dists, inds = neighbors.radius_neighbors(X=_query, radius=thresh)
+
+    #     if return_dists:
+    #         return inds, dists
+
+    #     return inds
+
+    # def _to_inds(self, ids):
+    #     if ids is None or isinstance(ids, np.ndarray):
+    #         return None
+
+    #     if self.config.patches_field is not None:
+    #         ids = self.label_ids
+    #     else:
+    #         ids = self.sample_ids
+
+    #     ids_map = {_id: i for i, _id in enumerate(ids)}
+    #     return np.array([ids_map[_id] for _id in ids])
+
+    # def _sort_by_similarity(
+    #     self, query, k, reverse, aggregation, return_dists
+    # ):
+    #     if query is None:
+    #         raise ValueError(
+    #             "A query must be provided when using aggregate similarity"
+    #         )
+
+    #     if aggregation not in _AGGREGATIONS:
+    #         raise ValueError(
+    #             "Unsupported aggregation method '%s'. Supported values are %s"
+    #             % (aggregation, tuple(_AGGREGATIONS.keys()))
+    #         )
+
+    #     sample_ids = self.current_sample_ids
+    #     label_ids = self.current_label_ids
+    #     keep_inds = self._current_inds
+    #     patches_field = self.config.patches_field
+    #     metric = self.config.metric
+
+    #     if isinstance(query, np.ndarray):
+    #         # Query by vectors
+    #         query_embeddings = query
+
+    #         index_embeddings = self._embeddings
+    #         if keep_inds is not None:
+    #             index_embeddings = index_embeddings[keep_inds]
+
+    #         dists = skm.pairwise_distances(
+    #             index_embeddings, query_embeddings, metric=metric
+    #         )
+    #     else:
+    #         # Query by IDs
+    #         query_ids = query
+
+    #         # Parse query IDs (always using full index)
+    #         if patches_field is None:
+    #             ids = self.sample_ids
+    #         else:
+    #             ids = self.label_ids
+
+    #         bad_ids = []
+    #         query_inds = []
+    #         for query_id in query_ids:
+    #             _inds = np.where(ids == query_id)[0]
+    #             if _inds.size == 0:
+    #                 bad_ids.append(query_id)
+    #             else:
+    #                 query_inds.append(_inds[0])
+
+    #         if bad_ids:
+    #             raise ValueError(
+    #                 "Query IDs %s were not included in this index" % bad_ids
+    #             )
+
+    #         # Perform query
+    #         self._ensure_neighbors()
+    #         dists = self._neighbors_helper.get_distances()
+    #         if dists is not None:
+    #             # Use pre-computed distances
+    #             if keep_inds is not None:
+    #                 dists = dists[keep_inds, :]
+
+    #             dists = dists[:, query_inds]
+    #         else:
+    #             # Compute distances from embeddings
+    #             index_embeddings = self._embeddings
+    #             if keep_inds is not None:
+    #                 index_embeddings = index_embeddings[keep_inds]
+
+    #             query_embeddings = self._embeddings[query_inds]
+    #             dists = skm.pairwise_distances(
+    #                 index_embeddings, query_embeddings, metric=metric
+    #             )
+
+    #     agg_fcn = _AGGREGATIONS[aggregation]
+    #     dists = agg_fcn(dists, axis=1)
+
+    #     inds = np.argsort(dists)
+    #     if reverse:
+    #         inds = np.flip(inds)
+
+    #     if k is not None:
+    #         inds = inds[:k]
+
+    #     if patches_field is not None:
+    #         ids = label_ids
+    #     else:
+    #         ids = sample_ids
+
+    #     if return_dists:
+    #         return ids[inds], dists[inds]
+
+    #     return ids[inds]
+
+    # def _ensure_neighbors(self):
+    #     if self._neighbors_helper is None:
+    #         self._neighbors_helper = NeighborsHelper(
+    #             self._embeddings, self.config.metric
+    #         )
+
+    # def _get_neighbors(self, full=False):
+    #     self._ensure_neighbors()
+
+    #     keep_inds = None if full else self._current_inds
+    #     return self._neighbors_helper.get_neighbors(keep_inds=keep_inds)
+
+    @staticmethod
+    def _parse_data(
+        samples,
+        config,
+        embeddings=None,
+        sample_ids=None,
+        label_ids=None,
+    ):
+        if embeddings is None:
+            embeddings, sample_ids, label_ids = fbu.get_embeddings(
+                samples._dataset,
+                patches_field=config.patches_field,
+                embeddings_field=config.embeddings_field,
+            )
+        elif sample_ids is None:
+            sample_ids, label_ids = fbu.get_ids(
+                samples,
+                patches_field=config.patches_field,
+                data=embeddings,
+                data_type="embeddings",
+            )
+
+        return embeddings, sample_ids, label_ids    
+
+    @classmethod
+    def _from_dict(cls, d, samples, config):
+        embeddings = d.get("embeddings", None)
+        if embeddings is not None:
+            embeddings = np.array(embeddings)
+
+        sample_ids = d.get("sample_ids", None)
+        if sample_ids is not None:
+            sample_ids = np.array(sample_ids)
+
+        label_ids = d.get("label_ids", None)
+        if label_ids is not None:
+            label_ids = np.array(label_ids)
+
+        index_name = d.get("index_name", None)
+        pod_type = d.get("pod_type", None)
+        pods = d.get("pods", None)
+        replicas = d.get("replias", None)
+        metric = d.get("metric", None)
+        upsert_pagination = d.get("upsert_pagination", None)
+        dimension = d.get("dimension", None)
+        api_key = d.get("api_key", None)
+        environment = d.get("environment", None)
+
+        return cls(
+            samples,
+            config,
+            embeddings=embeddings,
+            sample_ids=sample_ids,
+            label_ids=label_ids,
+            index_name=index_name,
+            pod_type=pod_type,
+            pods=pods,
+            replias=replicas,
+            metric=metric,
+            upsert_pagination=upsert_pagination,
+            dimension=dimension,
+            api_key=api_key,
+            environment=environment,
+        )
+    
+
+
+
+
+
 
 
 class SklearnSimilarityConfig(SimilarityConfig):
