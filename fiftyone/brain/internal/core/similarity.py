82e3d3a9517f7ef4ff91b858a3e58f7abc418012
brimoor <brimoor@umich.edu>
2023-03-07 22:56:20 -0500

    maintain old paths for backwards compatibility

---
+++ fiftyone/brain/internal/core/similarity.py
@@ -0,0 +1,14 @@
+"""
+Similarity methods.
+
+| Copyright 2017-2023, Voxel51, Inc.
+| `voxel51.com <https://voxel51.com/>`_
+|
+"""
+
+# Legacy import paths
+from fiftyone.brain.similarity import (
+    SimilarityConfig,
+    Similarity,
+    SimilarityIndex,
+)
