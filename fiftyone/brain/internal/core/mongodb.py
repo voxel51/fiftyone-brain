a0e56752d8c18cc11b38489fbe7bfaed4bfabafc
brimoor <brimoor@umich.edu>
2024-12-17 11:14:12 -0500

    lift restriction

--- fiftyone/brain/internal/core/mongodb.py
+++ fiftyone/brain/internal/core/mongodb.py
@@ -106,10 +106,10 @@ class MongoDBSimilarity(Similarity):
         # eg Atlas clusters generally have hostnames which end in "mongodb.net"
         # https://stackoverflow.com/q/73180110
         #
-        fou.ensure_package("pymongo>=4.7,<4.9")
+        fou.ensure_package("pymongo>=4.7")
 
     def ensure_usage_requirements(self):
-        fou.ensure_package("pymongo>=4.7,<4.9")
+        fou.ensure_package("pymongo>=4.7")
 
     def initialize(self, samples, brain_key):
         return MongoDBSimilarityIndex(
