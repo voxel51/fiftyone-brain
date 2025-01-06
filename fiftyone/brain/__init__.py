a82bd2ae37ee66f809ce84e1894df3aa182d82e6
Benjamin Kane <ben@voxel51.com>
2025-01-06 10:10:02 -0500

    2025 copyright (#225)

--- fiftyone/brain/__init__.py
+++ fiftyone/brain/__init__.py
@@ -4,7 +4,7 @@ and visualization.
 
 See https://github.com/voxel51/fiftyone for more information.
 
-| Copyright 2017-2024, Voxel51, Inc.
+| Copyright 2017-2025, Voxel51, Inc.
 | `voxel51.com <https://voxel51.com/>`_
 |
 """
